3a68ccb24a869e92c6760421acedc355d7030187
Artem Onyschenko <a.onyshchenko@cleack.com>
2024-03-29 13:35:29 +0200

    feat(master): Update DateTime value objects
    - Update DateTime

--- src/DateTime/DateTime.php
+++ src/DateTime/DateTime.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MicroModule\ValueObject\DateTime;
 
+use DateTimeZone;
 use MicroModule\ValueObject\DateTime\Exception\InvalidDateException;
 use MicroModule\ValueObject\ValueObjectInterface;
 use DateTime as BaseDateTime;
@@ -30,16 +31,18 @@ class DateTime implements ValueObjectInterface
     {
         $args = func_get_args();
 
-        if (!isset($args['1'])) {
-            if (is_string($args[0])) {
-                $dateTime = new BaseDateTime('@'.strtotime($args[0]));
-            } elseif (is_array($args[0])) {
-                $dateTime = new BaseDateTime('@'.strtotime($args[0][1]));
-            } else {
-                $dateTime = $args[0];
+        if (!isset($args[1])) {
+            if (!$args[0] instanceof BaseDateTime) {
+                if (is_array($args[0]) && isset($args[0]["date"]) && isset($args[0]["timezone"])) {
+                    $args[0] = new BaseDateTime($args[0]["date"], new DateTimeZone($args[0]["timezone"]));
+                } elseif (is_string($args[0])) {
+                    $args[0] = new BaseDateTime('@' . strtotime($args[0]));
+                } else {
+                    throw new Exception("Invalid date format!");
+                }
             }
 
-            return self::fromNativeDateTime($dateTime);
+            return self::fromNativeDateTime($args[0]);
         }
 
         $date = Date::fromNative($args[0], $args[1], $args[2]);
